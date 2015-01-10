6b816e20b34e7e40b6ddec29ae60cb28cf6d922a
vagrant <vagrant@localhost.localdomain>
2015-01-10 15:07:32 +0000

    initial commit. cut to only 1stpage from hop. not cool

---
+++ hopstepjump.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+# *-*coding:utf-8*-*
+
+from BeautifulSoup import BeautifulSoup
+#from HTMLParser import HTMLParser
+import re,urllib2
+
+
+pages = {
+	'hop':{
+		'url':'http://kaigoouen.net/program/gymnastics/gymnastics_2_1.html',
+		'page_count':25,
+	},
+	'step':{
+		'url':'http://kaigoouen.net/program/gymnastics/gymnastics_3_1.html',
+		'page_count':39,
+	},
+	'jump':{
+		'url':'http://kaigoouen.net/program/gymnastics/gymnastics_4_1.html',
+		'page_count':46,		  
+	}
+}
+
+
+if __name__ == '__main__':
+	page = pages['hop']
+	url = page['url']
+
+	htmldata = urllib2.urlopen(url)
+
+	soup = BeautifulSoup( unicode(htmldata.read(),'utf-8') )
+
+	print( soup.findAll('div',{'class':'hop_box'}) )
+
+	htmldata.close()
