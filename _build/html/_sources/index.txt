51e3614b2c99384132b9ced5776a0a8b1dafa8dd
vagrant <vagrant@localhost.localdomain>
2015-01-15 12:29:27 +0000

    sphinx model ok,hopstepjump.py editing

--- _build/html/_sources/index.txt
+++ _build/html/_sources/index.txt
@@ -9,8 +9,9 @@ Welcome to Rehab's documentation!
 Contents:
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 3
 
+   hop
 
 
 Indices and tables
--- hopstepjump.py
+++ hopstepjump.py
@@ -2,9 +2,10 @@
 # *-*coding:utf-8*-*
 
 from BeautifulSoup import BeautifulSoup
-#from HTMLParser import HTMLParser
 import re,urllib2
 
+base_url = 'http://kaigoouen.net/'
+image_url = base_url + 'img/'
 
 pages = {
 	'hop':{
@@ -21,7 +22,21 @@ pages = {
 	}
 }
 
+#### support ####
+br = "\n"
+def header_line(txt):
+	return "="*len(txt)*2 + br
 
+#### for make .rst files ####
+def sphinx_title(txt):
+	title_line = header_line(txt)
+	return txt + br + title_line + br
+
+def sphinx_body(img,txt):
+	pass
+
+
+#### main ####
 if __name__ == '__main__':
 	page = pages['hop']
 	url = page['url']
@@ -30,6 +45,10 @@ if __name__ == '__main__':
 
 	soup = BeautifulSoup( unicode(htmldata.read(),'utf-8') )
 
-	print( soup.findAll('div',{'class':'box02'}) )
+	box = soup.findAll('div',{'class':'box02'})[1]
+
+	title = box.contents[1].contents[0]['alt']
+	print( sphinx_title(title) )
+
 
 	htmldata.close()
