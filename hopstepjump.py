5dbf317fe5a2827ad9e478bc7cc214ffbf1057f2
vagrant <vagrant@localhost.localdomain>
2015-02-02 09:06:14 +0000

    all contents change to utf-8, write file from '>' is ok

--- hopstepjump.py
+++ hopstepjump.py
@@ -12,44 +12,47 @@ hopstepjump = {
 	},
 	'step':{
 		'index':3,
-		'last_page':0,
+		'last_page':39,
 	},
 	'jump':{
 		'index':4,
-		'last_page':0,
+		'last_page':46,
 	},
 }
 
 #### support ####
-br = "\n"
+br = u"\n"
 def header_line(txt):
-	return "="*len(txt)*2 + br
+	return u"="*len(txt)*2 + br
 
 #### for make .rst files ####
 def sphinx_title(txt):
 	title_line = header_line(txt)
-	return br + txt + br + title_line + br
+	return br.join([br,txt,title_line]).encode('utf-8')
 
 def sphinx_body_image(name,url,txt):
-	name = str('|%s|' % name)	# strしないとunicode型になりUnicodeDecodeError
+	name = u'|%s|' % name
 
-	image = '\n.. {name} image:: {url}\n'.format(name=name,url=url)
+	txt = txt.decode('utf-8')
 
-	option = '   :alt: 参考画像' + br*2	#
+	image = u'\n.. {name} image:: {url}\n'.format(name=name,url=url)
 
-	line = '='*len(name) + '  ' + '='*len(txt.decode('utf-8')) + br
+	option = u'   :alt: 参考画像' + br*2
 
-	# if '\n' in text: 改行前後で分けて' '*len(name)+'  |'の後に次の行
-	if '\n' in txt:
-		txts = txt.split('\n')
-		txt = reduce(lambda x,y: x + '\n{name_space}  |'.format( name_space=' '*len(name) ) + y,txts)
+	line = u'='*len(name) + u'  ' + u'='*len(txt) + br
 
-	return image + option + line + name + '  |' + txt + br + line
+	if br in txt:
+		txts = txt.splitlines()
+		txt = reduce(lambda x,y: x + u'\n{name_space}  |'.format( name_space=u' '*len(name) ) + y,txts)
+
+	return (image + option + line + name + u'  |' + txt + br + line).encode('utf-8')
 
 
 #### main ####
 if __name__ == '__main__':
-	page = hopstepjump['hop']
+	choice = 'hop'
+
+	page = hopstepjump[choice]
 
 	p = 1
 	while p <= page['last_page']:
@@ -61,15 +64,15 @@ if __name__ == '__main__':
 
 		for box in soup.findAll('div',{'class':'box02'}):
 
-			lessons = box.find('div',{'class':'box02_txt_hop'})
+			lessons = box.find('div',{'class':'box02_txt_%s' % choice})
 
 			if lessons is not None:
 
 				title = box.contents[1].contents[0]['alt']
 				print( sphinx_title(title) )
 
-				texts = lessons.findAll('div',{'class':'hop_txt'})
-				images = lessons.findAll('div',{'class':'hop_image'})
+				texts = lessons.findAll('div',{'class':'%s_txt' % choice})
+				images = lessons.findAll('div',{'class':'%s_image' % choice})
 				texts = iter(texts)
 
 				for image in images:
